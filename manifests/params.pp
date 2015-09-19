db9bd63fd1445e9c7fc370d2fdf559a2092675b2
Thomas Sturm <ths@nerdli.ch>
2015-09-19 22:24:22 +0200

    config template update to latest version

--- manifests/params.pp
+++ manifests/params.pp
@@ -56,14 +56,9 @@ class plexmediaserver::params {
   # General stuff
   $plex_user                                       = 'plex'
   $plex_media_server_home                          = '/usr/lib/plexmediaserver'
-  $plex_media_server_application_support_dir_array = [
-    '`getent passwd $PLEX_USER|awk -F : \'{print $6}\'`',
-    '/Library/Application Support'
-  ]
-  $plex_media_server_application_support_dir =
-    join($plex_media_server_application_support_dir_array, ' ')
+  $plex_media_server_application_support_dir = '${HOME}/Library/Application Support'
   $plex_media_server_max_plugin_procs        = '6'
-  $plex_media_server_max_stack_size          = '10000'
+  $plex_media_server_max_stack_size          = '3000'
   $plex_media_server_max_lock_mem            = '3000'
   $plex_media_server_max_open_files          = '4096'
   $plex_media_server_tmpdir                  = '/tmp'
